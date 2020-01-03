b703b7ad3131a36d4ee3a1cfbd17fdaf9715e157
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-03 12:34:40 +0900

    fix warnings

--- src/main.ml
+++ src/main.ml
@@ -1,11 +1,11 @@
 open GMain
-
-let main () = 
+   
+let main () =
   begin
-    let locale = GtkMain.Main.init () in
+    let _ = GtkMain.Main.init () in
     let window = GWindow.window ~width:320 ~height:100
                    ~title:"Simple lablgtk program" () in
-    window#connect#destroy ~callback:Main.quit;
+    ignore @@ window#connect#destroy ~callback:Main.quit;
     window#show ();
     Main.main ();
   end;;
