1eb2f07ca637e08cd4b62a5031c8e81f3b8ab227
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-14 20:34:32 +0900

    modify lib name

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,9 @@ set -ex
 
 sh setup-opam
 eval $(opam env)
-sed -i.bak "s/lablgtk2/$LABLGTK/" src/dune
+if [ "$LABLGTK" != "lablgtk" ]; then
+    sed -i.bak "s/lablgtk2/$LABLGTK/" src/dune
+fi
 dune build src/main.exe
 
 if [ "$(uname)" = Darwin ]; then
