85d646d3ded27e6f761794c823d3ba234a572341
Takashi Masuyama <mamewotoko@gmail.com>
2021-04-15 21:26:00 +0900

    output script trace

--- ci/build.sh
+++ ci/build.sh
@@ -12,10 +12,14 @@ dune build src/main.exe
 if [ "$(uname)" = Darwin ]; then
     exit 0
 fi
-    
-curl -u $AUTH -fsSLO https://mamewo.ddo.jp/private/gdrive
-chmod +x gdrive
+
 curl -fsSLO https://raw.githubusercontent.com/mamewotoko/xvfb-screenshooter/master/xvfb-screenshooter.sh
 chmod +x xvfb-screenshooter.sh
 ./xvfb-screenshooter.sh timeout 10s dune exec src/main.exe
-sh -x ci/upload.sh
+
+if [ -n "$UPLOAD_SCREENSHOT" ]; then
+    curl -u $AUTH -fsSLO https://mamewo.ddo.jp/private/gdrive
+    chmod +x gdrive
+    sh -x ci/upload.sh
+fi
+
---
+++ ci/install_ci_tools.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+sudo apt update
+sudo apt install -y xvfb x11-apps imagemagick ffmpeg
