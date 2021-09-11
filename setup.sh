070ded41561ae4373e2b3a9c6ce84faa200e5b46
Takashi Masuyama <mamewotoko@gmail.com>
2021-09-11 21:02:54 +0900

    add check

--- setup.sh
+++ setup.sh
@@ -6,14 +6,38 @@ opam init -y --disable-sandboxing --auto-setup
 eval $(opam env)
 opam update
 opam switch list-available ocaml-base-compiler
-#opam switch create 4.12.0 
+#opam switch create 4.12.0
 echo 'eval $(opam env)' >> ~/.bashrc
 
-# install libraries 
-./setup-opam
+if [ -z "$LABLGTK" ]; then
+    LABLGTK=lablgtk3
+fi
 
-opam install -y dune
+if [ "$(uname)" = Darwin ]; then
+    case "$LABLGTK" in
+        lablgtk)
+            brew install gtk+;;
+        lablgtk3)
+            brew install gtk+3;;
+    esac
+else
+    # ubuntu
+    case "$LABLGTK" in
+        lablgtk)
+            sudo apt-get install -y libgtk2.0-dev
+            sed -i -e 's/lablgtk3/lablgtk2/' src/dune
+            ;;
+        lablgtk3)
+            sudo apt-get install -y libexpat1-dev libgtk-3-dev pkg-config
+            ;;
+    esac
+fi
 
+opam install -y depext
+opam depext -y conf-m4.1
+opam install -y $LABLGTK ocamlfind dune
+
+# eval $(opam env)
 # build
 # dune build src/main.exe
 # dune run src/main.exe
